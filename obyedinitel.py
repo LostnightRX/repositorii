b647c382d71dfda1d23cad1e9c9ca7988e9daafe
mikhail <95572536+LostnightRX@users.noreply.github.com>
2022-12-16 11:56:43 +0700

    Add files via upload

---
+++ obyedinitel.py
@@ -0,0 +1,70 @@
+def systems():
+    N10 = int(input('введите число '))
+    p = int(input("введите сс "))
+    Np = 0
+    k = 1
+    while N10 != 0:
+        Np = Np + (N10 % p) * k
+        k = k * 10
+        N10 = N10 // p
+    print(Np)
+def numSys():
+    p = int(input())
+    Np = int(input())
+    k = int(1)
+    N10 = int(0)
+    while p != 0:
+        N10 = N10 + (Np % 10) * k
+        k = k * p
+        Np = Np // 10
+        p = p - 1
+    print("N10= ", N10)
+    return
+
+def operation():#Сообщение от Сола Гудмана#
+    print("введите p (2 < p <= 10): ")
+    p = int(input())
+    for X in range(1, p - 1):
+        for Y in range(1, p - 1):
+            Z = (X * X / p) * 10 + (X * Y % p)
+            print(f'{Z:03}')
+        print("\n")
+        return
+    return
+def distance(x,y):
+    k=7
+    for i in range(7):
+        if x%10==y%10:
+            k=k-1
+        x=x//10
+        y=y//10
+    return k
+def hemming(): 
+    a='0123456789'
+    nums=list(a)
+    print(nums)
+        
+    ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+    b='0000000 000111 0010110 0011001 0100101 0101010 0110011 0111100 1000011 1001100'
+    hem = b.split()
+    print(hem)
+    for i in range(len(hem)):
+        hem[i]=int(hem[i])
+    print(hem)        
+    
+    cod=int(input("код="))
+
+    min=distance(cod,hem[0])
+    imin=0
+    for i in range(10):
+        D=distance(cod,hem[i])
+        if D:
+            min=D
+            imin=i
+    if min==0:
+        print(f"Код верный: символ {nums[imin]}")
+    elif min==1:
+        print(f"Код исправлен: символ {nums[imin]}")
+    else:
+        print(f"Код неверный")
