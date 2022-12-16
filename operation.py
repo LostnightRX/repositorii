82434c32dcdc7fd1dd55d8196568113842e257a8
mikhail <95572536+LostnightRX@users.noreply.github.com>
2022-12-16 11:59:01 +0700

    Delete operation.py

--- operation.py
+++
@@ -1,8 +0,0 @@
-#Сообщение от Сола Гудмана#
-print("введите p (2 < p <= 10): ")
-p = int(input())
-for X in range(1, p - 1):
-    for Y in range(1, p - 1):
-        Z = (X * X / p) * 10 + (X * Y % p)
-        print(f'{Z:03}')
-    print("\n")
