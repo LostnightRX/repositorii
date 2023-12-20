bb26f57134f459abbc759de6e6df215f11f50f9a
mikhail <95572536+LostnightRX@users.noreply.github.com>
2023-12-20 20:24:10 +0700

    Update 11244.py

--- kompege/11244.py
+++ kompege/11244.py
@@ -1,17 +1,21 @@
-with open('27_A_11244.txt') as f:
-    k, n, *a = map(int, f.readlines())
-res = 0
-for i in range(len(a)):
-    for j in range(i + k, len(a), k):
-        res = max(res, a[i]*a[j])
-print(res)
+def A():
+    with open('27_A_11244.txt') as f:
+        k, n, *a = map(int, f.readlines())
+    res = 0
+    for i in range(len(a)):
+        for j in range(i + k, len(a), k):
+            res = max(res, a[i]*a[j])
+    print(res)
 
-with open('27_B_11244.txt') as f:
-    k, n, *a = map(int, f.readlines())
-res = 0
-g = []
-for i in range(17):
-    b = [a[x] for x in range(i, len(a), k)]
-    g.append(b.pop(b.index(max(b))) * b.pop(b.index(max(b))))
-    g.append(b.pop(b.index(min(b))) * b.pop(b.index(min(b))))
-print(max(g))
+def B():
+    with open('27_B_11244.txt') as f:
+        k, n, *a = map(int, f.readlines())
+    res = 0
+    for i in range(17):
+        b = [a[x] for x in range(i, len(a), k)]
+        res = max(b.pop(b.index(max(b))) * b.pop(b.index(max(b))), b.pop(b.index(min(b))) * b.pop(b.index(min(b))))
+    print(res)
+
+import timeit
+print(timeit.timeit(A, number=1))
+print(timeit.timeit(B, number=1))
